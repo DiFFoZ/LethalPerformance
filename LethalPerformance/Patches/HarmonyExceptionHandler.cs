798ad29a2e5f3e1097cfd96a8e5e1e0290a07df5
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-06-26 18:40:59 +0700

    Add harmony exception handler

---
+++ LethalPerformance/Patches/HarmonyExceptionHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LethalPerformance.Patches;
+internal static class HarmonyExceptionHandler
+{
+    public static Exception? ReportException(Exception? exception)
+    {
+        LethalPerformancePlugin.Instance.Logger.LogError(exception);
+        return null;
+    }
+}
--- LethalPerformance/Patches/Patch_HDCamera.cs
+++ LethalPerformance/Patches/Patch_HDCamera.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using System;
+using HarmonyLib;
 using Unity.Burst;
 using UnityEngine.Rendering.HighDefinition;
 
@@ -8,14 +9,18 @@ internal static unsafe class Patch_HDCamera
 {
     private static readonly Testing.TestDelegate s_TestDelegate = BurstCompiler.CompileFunctionPointer<Testing.TestDelegate>(Testing.Test).Invoke;
 
+    [HarmonyCleanup]
+    public static Exception? Cleanup(Exception exception)
+    {
+        return HarmonyExceptionHandler.ReportException(exception);
+    }
+
     [HarmonyPrefix]
     public static unsafe bool Prefix(HDCamera __instance, ref ShaderVariablesXR cb)
     {
         fixed (void* p = &cb, views = __instance.m_XRViewConstants)
         {
             s_TestDelegate((Testing.ReadableViewConstants*)views, __instance.viewCount, (Testing.ReadableShaderVariablesXR*)p);
-
-            cb = *(ShaderVariablesXR*)p;
             return false;
         }
     }
