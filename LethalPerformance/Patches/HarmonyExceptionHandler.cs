93a598ab9cf41263548bae7fdfe3553a3ddde899
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-06-26 19:11:46 +0700

    Do not report null exception

--- LethalPerformance/Patches/HarmonyExceptionHandler.cs
+++ LethalPerformance/Patches/HarmonyExceptionHandler.cs
@@ -5,7 +5,11 @@ internal static class HarmonyExceptionHandler
 {
     public static Exception? ReportException(Exception? exception)
     {
-        LethalPerformancePlugin.Instance.Logger.LogError(exception);
+        if (exception != null)
+        {
+            LethalPerformancePlugin.Instance.Logger.LogError(exception);
+        }
+        
         return null;
     }
 }
