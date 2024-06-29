775e5955b57fabfeb909fcb6917e222c2b458416
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-06-29 14:56:24 +0700

    Remove `DisableUICamera` config option
    Probably in future I will continue to work on that

--- LethalPerformance/ConfigManager.cs
+++ LethalPerformance/ConfigManager.cs
@@ -14,17 +14,9 @@ internal class ConfigManager
 
 #nullable disable
 
-    public ConfigEntry<bool> DisableUICamera { get; private set; }
-
 #nullable restore
 
     private void BindConfig()
     {
-        DisableUICamera = m_Config.Bind("Experimental", "DisableUICamera", false,
-            """
-            Disables UI camera to improve CPU time.
-            Note: this option will make game to render in native resolution (meaning that settings via LCUltraWide will be ignored), so if you're GPU-bound I would not recommend to use this option.
-            Incompatibilities with mods: LCVR (because it already disables UICamera).
-            """);
     }
 }
