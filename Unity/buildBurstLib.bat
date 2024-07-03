5b7d92112dc1040558ba95c327fa1805def5d6dd
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-07-03 15:00:50 +0700

    Move build burst lib bat out of unity folder

--- Unity/buildBurstLib.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\Unity\Hub\Editor\2022.3.9f1\Editor\Unity.exe" -quit -batchmode -executeMethod BuildAction.BuildGame
-@echo off
---
+++ buildBurstLib.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files\Unity\Hub\Editor\2022.3.9f1\Editor\Unity.exe" -projectPath "Unity" -quit -batchmode -executeMethod BuildAction.BuildGame -logFile -
+pause
