dca79aeff093f7bfc4ae9d4d9d6185280ff20457
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-07-04 12:09:54 +0700

    Optimize StartOfRound.SetPlayerSafeInShip

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.0.6] 2024-07-02
+### Added
+- Optimization of `HDCamera.UpdateShaderVariablesGlobalCB`.
+### Fixed
+- Harmony patching exception with Loadstone mod.
+
 ## [0.0.5] 2024-06-29
 ### Fixed
 - Temp bandaid fix for custom moons that item drop ship throwing NullReferenceException (important, it still doesn't fix spawning of vehicle on custom moons).
---
+++ LethalPerformance/Patches/Patch_StartOfRound.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using LethalPerformance.API;
+using Object = UnityEngine.Object;
+
+namespace LethalPerformance.Patches;
+[HarmonyPatch(typeof(StartOfRound))]
+internal static class Patch_StartOfRound
+{
+    [HarmonyCleanup]
+    public static Exception? Cleanup(Exception exception)
+    {
+        return HarmonyExceptionHandler.ReportException(exception);
+    }
+
+    [HarmonyPatch(nameof(StartOfRound.SetPlayerSafeInShip))]
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> ReplaceFindOfObjectOfType(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        var matcher = new CodeMatcher(instructions);
+
+        // locals
+        var countLocal = generator.DeclareLocal(typeof(int), false);
+
+        var findObjectOfTypeMethod = typeof(Object)
+           .GetMethod(nameof(Object.FindObjectsOfType), 1, AccessTools.all, null, CallingConventions.Any, [], [])
+           .MakeGenericMethod(typeof(EnemyAI));
+
+        var roundManagerInstanceGetter = typeof(RoundManager)
+                .GetProperty(nameof(RoundManager.Instance), AccessTools.all)
+                .GetGetMethod();
+
+        var spawnedEnemiesField = typeof(RoundManager).GetField(nameof(RoundManager.SpawnedEnemies), AccessTools.all);
+
+        var listInternalArrayField = typeof(List<EnemyAI>)
+            .GetField("_items", AccessTools.all);
+
+        var listGetCountMethod = typeof(List<EnemyAI>)
+            .GetProperty(nameof(List<EnemyAI>.Count), AccessTools.all)
+            .GetGetMethod();
+
+        matcher.SearchForward(c => c.Calls(findObjectOfTypeMethod))
+            .Set(OpCodes.Call, roundManagerInstanceGetter)
+            .Advance(1)
+            .InsertAndAdvance(new(OpCodes.Ldfld, spawnedEnemiesField),
+            new(OpCodes.Ldfld, listInternalArrayField)) // unsafe, but allows to other mods to transpiler this method
+            .Advance(1) // move after stfld array
+            .InsertAndAdvance(new(OpCodes.Ldloc_1), // store count to local var
+            new(OpCodes.Callvirt, listGetCountMethod),
+            new(OpCodes.Stloc, countLocal));
+
+        // replace array.Length to local var Count
+        matcher.MatchForward(false, new(OpCodes.Ldloc_1), new(OpCodes.Ldlen))
+            .Repeat(m =>
+            {
+                m.RemoveInstructions(2)
+                .InsertAndAdvance(new CodeInstruction(OpCodes.Ldloc, countLocal));
+            });
+
+        return matcher.InstructionEnumeration();
+    }
+}
