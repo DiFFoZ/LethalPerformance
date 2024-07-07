4bf5816bd4202f1b088d94a8228a21505f5d187a
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-07-07 23:09:33 +0700

    Remove custom marker of get object fast

--- LethalPerformance/API/ObjectExtensions.cs
+++ LethalPerformance/API/ObjectExtensions.cs
@@ -1,22 +1,12 @@
 ﻿using UnityEngine;
-using UnityEngine.Pool;
-using UnityEngine.Profiling;
-using UnityEngine.SceneManagement;
 
 namespace LethalPerformance.API;
 internal static class ObjectExtensions
 {
     public static T? FindObjectByTypeNonOrdered<T>() where T : Object
     {
-#if ENABLE_PROFILER
-        Profiler.BeginSample("DiFFoZ.FindObjectFast." + typeof(T).Name);
-#endif
         var objects = Object.FindObjectsByType(typeof(T), FindObjectsInactive.Exclude, FindObjectsSortMode.None);
         var uObject = objects.Length > 0 ? objects[0] : null;
-
-#if ENABLE_PROFILER
-        Profiler.EndSample();
-#endif
 
         return uObject as T;
     }
