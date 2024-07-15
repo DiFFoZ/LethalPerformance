61153c7afafcafb68eb94ae777633a7774bbdc20
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-07-16 06:39:42 +0700

    Patch HangarShipDoor to not allocate when updating text

---
+++ LethalPerformance/Patches/Patch_HangarShipDoor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using TMPro;
+
+namespace LethalPerformance.Patches;
+[HarmonyPatch(typeof(HangarShipDoor))]
+internal static class Patch_HangarShipDoor
+{
+    [HarmonyPatch(nameof(HangarShipDoor.Update))]
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> FixFormatAllocation(IEnumerable<CodeInstruction> instructions)
+    {
+        var matcher = new CodeMatcher(instructions);
+
+        // todo: add check if text should be updated?
+
+        matcher.MatchForward(false,
+            new(OpCodes.Box),
+            new(OpCodes.Call),
+            new(OpCodes.Callvirt))
+            .SetInstructionAndAdvance(new CodeInstruction(OpCodes.Conv_R4)) // convert int to float
+            .RemoveInstruction() // remove string.Format call
+            .Set(OpCodes.Call, typeof(TMP_Text).GetMethod(nameof(TMP_Text.SetText), [typeof(string), typeof(float)]));
+
+        return matcher.InstructionEnumeration();
+    }
+}
