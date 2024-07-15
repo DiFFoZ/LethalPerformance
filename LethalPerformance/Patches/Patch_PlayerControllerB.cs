7c91acf707de0850157c14654367b7f2eac885ac
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-07-16 05:58:36 +0700

    Fix local username billboard disables/enables every frame

---
+++ LethalPerformance/Patches/Patch_PlayerControllerB.cs
@@ -0,0 +1,21 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace LethalPerformance.Patches;
+[HarmonyPatch(typeof(PlayerControllerB))]
+internal static class Patch_PlayerControllerB
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(PlayerControllerB.ShowNameBillboard))]
+    public static bool FixLocalBillBoardIsEnabling(PlayerControllerB __instance, bool __runOriginal)
+    {
+        // todo: remove CanvasScaler and GraphicRaycaster from billboard
+        if (!__runOriginal)
+        {
+            return __runOriginal;
+        }
+
+        var isLocalPlayer = __instance == StartOfRound.Instance.localPlayerController;
+        return !isLocalPlayer;
+    }
+}
