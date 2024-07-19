71c3e771b5b569ea08a0c8b5dbee7f283d8cf411
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-07-19 10:16:19 +0700

    Make finding of DissonanceComms fast

--- LethalPerformance/Patches/FindingObjectOptimization/NativeFindObjectOfTypePatch.cs
+++ LethalPerformance/Patches/FindingObjectOptimization/NativeFindObjectOfTypePatch.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using Dissonance;
 using HarmonyLib;
 using UnityEngine;
 
@@ -9,18 +10,20 @@ namespace LethalPerformance.Patches.FindingObjectOptimization;
 [HarmonyPatch]
 internal static class NativeFindObjectOfTypePatch
 {
-    private static readonly Dictionary<Type, Func<Object>> s_MapGettingInstance = new()
+    private static readonly Dictionary<Type, Func<Object?>> s_MapGettingInstance = new()
     {
-        { typeof(StartOfRound), () => StartOfRound.Instance },
-        { typeof(TimeOfDay), () => TimeOfDay.Instance },
-        { typeof(GameNetworkManager), () => GameNetworkManager.Instance },
-        { typeof(HUDManager), () => HUDManager.Instance },
-        { typeof(GlobalEffects), () => GlobalEffects.Instance },
-        { typeof(IngamePlayerSettings), () => IngamePlayerSettings.Instance },
-        { typeof(RoundManager), () => RoundManager.Instance },
-        { typeof(ShipBuildModeManager), () => ShipBuildModeManager.Instance },
-        { typeof(SoundManager), () => SoundManager.Instance },
-        { typeof(SteamManager), () => SteamManager.Instance },
+        [typeof(StartOfRound)] = () => StartOfRound.Instance,
+        [typeof(TimeOfDay)] = () => TimeOfDay.Instance,
+        [typeof(GameNetworkManager)] = () => GameNetworkManager.Instance,
+        [typeof(HUDManager)] = () => HUDManager.Instance,
+        [typeof(GlobalEffects)] = () => GlobalEffects.Instance,
+        [typeof(IngamePlayerSettings)] = () => IngamePlayerSettings.Instance,
+        [typeof(RoundManager)] = () => RoundManager.Instance,
+        [typeof(ShipBuildModeManager)] = () => ShipBuildModeManager.Instance,
+        [typeof(SoundManager)] = () => SoundManager.Instance,
+        [typeof(SteamManager)] = () => SteamManager.Instance,
+        // dissonance comms is also used in main menu, so checking for null here
+        [typeof(DissonanceComms)] = () => StartOfRound.Instance != null ? StartOfRound.Instance.voiceChatModule : null!,
     };
 
     [HarmonyTargetMethods]
@@ -28,10 +31,11 @@ internal static class NativeFindObjectOfTypePatch
     {
         yield return typeof(Object).GetMethod(nameof(Object.FindObjectOfType), [typeof(Type), typeof(bool)]);
         yield return typeof(Object).GetMethod(nameof(Object.FindAnyObjectByType), [typeof(Type), typeof(FindObjectsInactive)]);
+        yield return typeof(Object).GetMethod(nameof(Object.FindFirstObjectByType), [typeof(Type), typeof(FindObjectsInactive)]);
     }
 
     [HarmonyPrefix]
-    public static bool FindObjectFast(Type type, ref Object __result)
+    public static bool FindObjectFast(Type type, ref Object? __result)
     {
         if (s_MapGettingInstance.TryGetValue(type, out var getter))
         {
