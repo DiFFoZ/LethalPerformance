19269389f131a9f719c750521280204aea782718
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-07-28 14:29:50 +0700

    Add open logs keybind for debug configuration

---
+++ LethalPerformance/Utilities/OpenLogsFolderKeybind.cs
@@ -0,0 +1,57 @@
+﻿using LethalPerformance.API;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+namespace LethalPerformance.Utilities;
+internal class OpenLogsFolderKeybind
+{
+    private static InputActionAsset? s_InputActions;
+
+#if ENABLE_PROFILER
+    [InitializeOnAwake]
+    private static void Initialize()
+    {
+        var asset = s_InputActions = ScriptableObject.CreateInstance<InputActionAsset>();
+        asset.name = "LethalPerformance Input Asset";
+        asset.hideFlags = HideFlags.HideAndDontSave;
+        Object.DontDestroyOnLoad(asset);
+
+        var actionMap = asset.AddActionMap("LPActionMap");
+        var action = actionMap.AddAction("General", InputActionType.Button);
+
+        action.AddCompositeBinding("TwoModifiers")
+            .With("Binding", "<Keyboard>/l")
+            .With("Modifier1", "<Keyboard>/ctrl")
+            .With("Modifier2", "<Keyboard>/shift");
+
+        action.performed += OnOpenLogsPressed;
+
+        SceneManager.activeSceneChanged += SceneManager_activeSceneChanged;
+    }
+#endif
+
+    private static void SceneManager_activeSceneChanged(Scene arg0, Scene arg1)
+    {
+        SceneManager.activeSceneChanged -= SceneManager_activeSceneChanged;
+
+        // deferred initialization of input action
+        if (s_InputActions != null)
+        {
+            s_InputActions.Enable();
+        }
+    }
+
+    private static void OnOpenLogsPressed(InputAction.CallbackContext obj)
+    {
+        if (GameNetworkManager.Instance != null
+            && GameNetworkManager.Instance.localPlayerController != null
+            && GameNetworkManager.Instance.localPlayerController.isTypingChat)
+        {
+            return;
+        }
+
+        // wrap it with "" because of spacing
+        Application.OpenURL($"\"{Application.persistentDataPath}\"");
+    }
+}
