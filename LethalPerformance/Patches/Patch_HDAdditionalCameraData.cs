e98cd7b50068c31d8fc131ea20ef7da9357a644d
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-07-28 07:21:00 +0700

    Do not register camera debug window hierarchy

---
+++ LethalPerformance/Patches/Patch_HDAdditionalCameraData.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using UnityEngine.Rendering.HighDefinition;
+
+namespace LethalPerformance.Patches;
+[HarmonyPatch(typeof(HDAdditionalCameraData))]
+internal static class Patch_HDAdditionalCameraData
+{
+    [HarmonyPatch(nameof(HDAdditionalCameraData.RegisterDebug))]
+    [HarmonyPrefix]
+    public static bool DisableRegisteringDebugData()
+    {
+        // why on earth Unity registering debug hierarchy window even if you cannot access them (in release build).
+        return false;
+    }
+}
