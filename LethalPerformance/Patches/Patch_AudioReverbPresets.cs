4e6c6e310338eb611bfcd0dcfa5e0e4826946edc
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-09-01 16:45:11 +0700

    Cache latest AudioReverbPresets

---
+++ LethalPerformance/Patches/Patch_AudioReverbPresets.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using LethalPerformance.Utilities;
+
+namespace LethalPerformance.Patches;
+[HarmonyPatch(typeof(AudioReverbPresets))]
+internal static class Patch_AudioReverbPresets
+{
+    private static readonly UnsafeCachedInstance<AudioReverbPresets> s_Instance = new();
+
+    static Patch_AudioReverbPresets()
+    {
+        UnsafeCacheManager.AddReferenceToMap(s_Instance);
+    }
+
+    [HarmonyPatch("Awake")] // was added by preloader
+    [HarmonyPostfix]
+    public static void Awake(AudioReverbPresets __instance)
+    {
+        s_Instance.Instance = __instance;
+    }
+}
