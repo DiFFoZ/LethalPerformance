83e39e30ae51bdcb957755b2b6753527d617be3a
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-11-30 15:32:54 +0700

    Cache normals array on calculate

--- LethalPerformance/Patches/Patch_PlayerControllerB.cs
+++ LethalPerformance/Patches/Patch_PlayerControllerB.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
 using GameNetcodeStuff;
 using HarmonyLib;
 using LethalPerformance.Patcher.API;
+using UnityEngine;
 
 namespace LethalPerformance.Patches;
 [HarmonyPatch(typeof(PlayerControllerB))]
 internal static class Patch_PlayerControllerB
 {
+    private static Vector3[] s_Normals = null!;
+
     [HarmonyCleanup]
     public static Exception? Cleanup(Exception exception)
     {
@@ -27,5 +33,34 @@ internal static class Patch_PlayerControllerB
 
         var isLocalPlayer = __instance == StartOfRound.Instance.localPlayerController;
         return !isLocalPlayer;
+    }
+
+    [HarmonyPatch(nameof(PlayerControllerB.CalculateGroundNormal))]
+    [HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> UseStaticField(IEnumerable<CodeInstruction> instructions)
+    {
+        var list = instructions.ToList();
+
+        var ldc0 = OpCodes.Ldc_I4_0.Value;
+
+        var opcode = list[0].opcode;
+        var diff = opcode.Value - ldc0;
+
+        // failed to find length of array
+        if (diff < 0 || diff >= OpCodes.Ldc_I4_8.Value)
+        {
+            return list;
+        }
+
+        s_Normals = new Vector3[diff + 1];
+
+        // ldc.i4.5
+        // newarr
+        list.RemoveRange(0, 2);
+
+        var normalsField = typeof(Patch_PlayerControllerB).GetField(nameof(s_Normals), AccessTools.all);
+        list.Insert(0, new(OpCodes.Ldsfld, normalsField));
+
+        return list;
     }
 }
