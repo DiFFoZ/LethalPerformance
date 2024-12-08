553b5982a4e85eb139f0583fb1ad19791a6614d4
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-12-08 09:00:01 +0700

    Add writer profiler samples

---
+++ LethalPerformance/Metrics/Patch_ES3Reader.cs
@@ -0,0 +1,47 @@
+﻿#if ENABLE_PROFILER
+using System.Collections.Generic;
+using ES3Internal;
+using HarmonyLib;
+using Unity.Profiling;
+
+namespace LethalPerformance.Metrics;
+[HarmonyPatch]
+internal static class Patch_ES3Reader
+{
+    private static readonly Stack<ProfilerMarker> s_ProfilerStack = new();
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(ES3Reader), nameof(ES3Reader.Create), argumentTypes: [typeof(ES3Settings)])]
+    public static void Create(ES3Settings settings)
+    {
+        var sampleName = "DiFFoZ.ES3.CreateReader." + settings.path;
+
+        ProfilerMarker marker = new ProfilerMarker(sampleName);
+        marker.Begin();
+
+        s_ProfilerStack.Push(marker);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ES3Reader), nameof(ES3Reader.Create), argumentTypes: [typeof(ES3Settings)])]
+    public static void CreatePostfix(ES3Reader __result)
+    {
+        // if file doesn't exists then it returns null reader.
+        // if reader is null then end the profiler marker
+        if (__result == null)
+        {
+            Dispose();
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ES3JSONReader), nameof(ES3JSONReader.Dispose))]
+    public static void Dispose()
+    {
+        if (s_ProfilerStack.TryPop(out var marker))
+        {
+            marker.End();
+        }
+    }
+}
+#endif
---
+++ LethalPerformance/Metrics/Patch_ES3Writer.cs
@@ -0,0 +1,49 @@
+﻿#if ENABLE_PROFILER
+using System.Collections.Generic;
+using ES3Internal;
+using HarmonyLib;
+using Unity.Profiling;
+
+namespace LethalPerformance.Metrics;
+[HarmonyPatch]
+internal static class Patch_ES3Writer
+{
+    private static readonly Stack<ProfilerMarker> s_ProfilerStack = new();
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(ES3Writer), nameof(ES3Writer.Create),
+        argumentTypes: [typeof(ES3Settings), typeof(bool), typeof(bool), typeof(bool)])]
+    public static void Create(ES3Settings settings)
+    {
+        var sampleName = "DiFFoZ.ES3.CreateWriter." + settings.path;
+
+        ProfilerMarker marker = new ProfilerMarker(sampleName);
+        marker.Begin();
+
+        s_ProfilerStack.Push(marker);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ES3Writer), nameof(ES3Writer.Create),
+        argumentTypes: [typeof(ES3Settings), typeof(bool), typeof(bool), typeof(bool)])]
+    public static void CreatePostfix(ES3Writer __result)
+    {
+        // if file doesn't exists then it returns null reader.
+        // if reader is null then end the profiler marker
+        if (__result == null)
+        {
+            Dispose();
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ES3JSONWriter), nameof(ES3JSONWriter.Dispose))]
+    public static void Dispose()
+    {
+        if (s_ProfilerStack.TryPop(out var marker))
+        {
+            marker.End();
+        }
+    }
+}
+#endif
