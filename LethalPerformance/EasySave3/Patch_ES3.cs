5361d8472648894421bff45a0c680f3742c936f3
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2025-03-04 18:13:33 +0700

    Cleanup patch of es3

--- LethalPerformance/EasySave3/Patch_ES3.cs
+++ LethalPerformance/EasySave3/Patch_ES3.cs
@@ -5,26 +5,6 @@ namespace LethalPerformance.EasySave3;
 [HarmonyPatch(typeof(ES3))]
 internal static class Patch_ES3
 {
-    [HarmonyPatch("LethalPerformance_Load")] // added by preloader
-    [HarmonyPrefix]
-    internal static void LoadPatch(ES3Settings settings)
-    {
-        ES3Utilities.ForceToCache(settings);
-        ES3Utilities.LoadFileToCache(settings);
-
-        //LethalPerformancePlugin.Instance.Logger.LogFatal("[ES3 Load] " + settings.path);
-    }
-
-    [HarmonyPatch("LethalPerformance_Save")] // added by preloader
-    [HarmonyPrefix]
-    internal static void SavePatch(ES3Settings settings)
-    {
-        ES3Utilities.ForceToCache(settings);
-        ES3Utilities.LoadFileToCache(settings);
-
-        //LethalPerformancePlugin.Instance.Logger.LogFatal("[ES3 Save] " + settings.path);
-    }
-
     [HarmonyPatch(nameof(ES3.DeleteFile), [typeof(ES3Settings)])]
     [HarmonyPostfix]
     internal static void DeleteCachedFile(ES3Settings settings)
@@ -45,6 +25,8 @@ internal static class Patch_ES3
     [HarmonyPatch(nameof(ES3.GetKeys), [typeof(ES3Settings)])]
     [HarmonyPatch(nameof(ES3.KeyExists), [typeof(string), typeof(ES3Settings)])]
     [HarmonyPatch(nameof(ES3.DeleteKey), [typeof(string), typeof(ES3Settings)])]
+    [HarmonyPatch("LethalPerformance_Load")] // added by preloader
+    [HarmonyPatch("LethalPerformance_Save")] // added by preloader
     [HarmonyPrefix]
     internal static void UseCache(ES3Settings settings)
     {
