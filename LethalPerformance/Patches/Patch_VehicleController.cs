afffc38205d2d591c2d78830491e37f4aeeb9b30
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2025-03-06 16:57:54 +0700

    Fix VehicleController searches ItemDropship after delivery

---
+++ LethalPerformance/Patches/Patch_VehicleController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using LethalPerformance.Patcher.API;
+using static System.Reflection.Emit.OpCodes;
+
+namespace LethalPerformance.Patches;
+[HarmonyPatch(typeof(VehicleController))]
+internal static class Patch_VehicleController
+{
+    [HarmonyCleanup]
+    public static Exception? Cleanup(Exception exception)
+    {
+        return HarmonyExceptionHandler.ReportException(exception);
+    }
+
+    [HarmonyPatch(nameof(VehicleController.FixedUpdate))]
+    [HarmonyTranspiler]
+    internal static IEnumerable<CodeInstruction> NotSearchDropshipAfterDelivery(IEnumerable<CodeInstruction> instructions)
+    {
+        var matcher = new CodeMatcher(instructions);
+
+        matcher.MatchForward(true, [
+            new(Call), // StartOfRound.Instance
+            new(Ldfld), // inShipPhase
+            new(Brtrue, name: "branch")
+            ]);
+
+        var label = matcher.NamedMatch("branch").operand;
+        var hasBeenSpawnedField = typeof(VehicleController).GetField(nameof(VehicleController.hasBeenSpawned), AccessTools.all);
+
+        matcher.Advance(1)
+            .Insert([
+                new(Ldarg_0),
+                new(Ldfld, hasBeenSpawnedField),
+                new(Brtrue, label)
+            ]);
+
+        return matcher.Instructions();
+    }
+}
