035826ca92cacb0b851875f8696d21cd505faef4
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2025-03-26 17:11:43 +0700

    Fix patch

--- LethalPerformance/Patches/Patch_EntranceTeleport.cs
+++ LethalPerformance/Patches/Patch_EntranceTeleport.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using HarmonyLib;
+using LethalPerformance.Patcher.API;
 using static System.Reflection.Emit.OpCodes;
 
 namespace LethalPerformance.Patches;
 [HarmonyPatch(typeof(EntranceTeleport))]
 internal static class Patch_EntranceTeleport
 {
+    [HarmonyCleanup]
+    public static Exception? Cleanup(Exception exception)
+    {
+        return HarmonyExceptionHandler.ReportException(exception);
+    }
+
+    [HarmonyPatch(nameof(EntranceTeleport.Update))]
+    [HarmonyTranspiler]
     public static IEnumerable<CodeInstruction> DoNotSearchEntrances(IEnumerable<CodeInstruction> instructions)
     {
         var matcher = new CodeMatcher(instructions);
 
-        // todo: move stfld EntranceTeleport::checkForEnemiesInterval before the check
         // todo: listen to dungeon status change
+
+        var checkForEnemiesIntervalField = typeof(EntranceTeleport)
+            .GetField(nameof(EntranceTeleport.checkForEnemiesInterval), AccessTools.all);
 
         matcher.MatchForward(false, [
             new(Ldarg_0),
@@ -27,6 +37,12 @@ internal static class Patch_EntranceTeleport
             new(Stfld), // EntranceTeleport::gotExitPoint
             // ret (br to the end)
             ])
+            // store 1f to the EntranceTeleport::checkForEnemiesInterval
+            .Insert([
+                new(Ldarg_0),
+                new(Ldc_R4, 1f),
+                new(Stfld, checkForEnemiesIntervalField)
+                ])
             .Advance(3)
             .RemoveInstructions(6);
 
