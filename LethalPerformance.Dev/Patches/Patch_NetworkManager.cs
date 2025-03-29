fcc05618ee7aa28e45979408e46a6f5bff0617fb
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2025-03-29 09:35:03 +0700

    Increase timeout to 2 minutes

---
+++ LethalPerformance.Dev/Patches/Patch_NetworkManager.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Unity.Netcode;
+
+namespace LethalPerformance.Dev.Patches;
+[HarmonyPatch(typeof(NetworkManager))]
+internal static class Patch_NetworkManager
+{
+    [HarmonyPatch(nameof(NetworkManager.SetSingleton))]
+    [HarmonyPostfix]
+    public static void SetTimeout()
+    {
+        // 2 minutes will be fine with a lot of LLL extended mods, right??
+        NetworkManager.Singleton.NetworkConfig.LoadSceneTimeOut = 180;
+    }
+}
