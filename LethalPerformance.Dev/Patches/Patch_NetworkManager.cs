48586f580cd5123df85842bfd909692715fdbf8a
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2025-03-29 11:13:08 +0700

    fix transport heartbeat timeout in big modpack

--- LethalPerformance.Dev/Patches/Patch_NetworkManager.cs
+++ LethalPerformance.Dev/Patches/Patch_NetworkManager.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 
 namespace LethalPerformance.Dev.Patches;
 [HarmonyPatch(typeof(NetworkManager))]
@@ -10,6 +11,13 @@ internal static class Patch_NetworkManager
     public static void SetTimeout()
     {
         // 2 minutes will be fine with a lot of LLL extended mods, right??
-        NetworkManager.Singleton.NetworkConfig.LoadSceneTimeOut = 180;
+        NetworkManager.Singleton.NetworkConfig.LoadSceneTimeOut = 120;
+
+        if (NetworkManager.Singleton.NetworkConfig.NetworkTransport is UnityTransport transport)
+        {
+            transport.ConnectTimeoutMS = 120 * 1000;
+            transport.DisconnectTimeoutMS = 120 * 1000;
+            transport.HeartbeatTimeoutMS = 120 * 1000;
+        }
     }
 }
