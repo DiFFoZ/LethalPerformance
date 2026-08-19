37c79a293d4dcfe38d1b921e4bc8e54da300f4ac
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2026-08-19 14:17:14 +0700

    Make item spawn use cached random scrap spawn

---
+++ LethalPerformance/Patches/Patch_GrabbableObject.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using LethalPerformance.Patcher.API;
+
+namespace LethalPerformance.Patches;
+[HarmonyPatch(typeof(GrabbableObject))]
+internal static class Patch_GrabbableObject
+{
+    [HarmonyCleanup]
+    public static Exception? Cleanup(Exception exception)
+    {
+        return HarmonyExceptionHandler.ReportException(exception);
+    }
+
+    [HarmonyPatch(nameof(GrabbableObject.Start))]
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> UseCachedRandomSpawnScrap(IEnumerable<CodeInstruction> instructions)
+    {
+        var matcher = new CodeMatcher(instructions);
+
+        var findObjectOfTypeMethod = typeof(Object)
+          .GetMethod(nameof(Object.FindObjectsOfType), 1, AccessTools.all, null, CallingConventions.Any, [], [])
+          .MakeGenericMethod(typeof(RandomScrapSpawn));
+
+        matcher.SearchForward(c => c.Calls(findObjectOfTypeMethod))
+            .Operand = SymbolExtensions.GetMethodInfo(() => GetAssignedRandomScrapSpawn);
+
+        matcher.Insert([
+            new (OpCodes.Ldarg_0)
+            ]);
+
+        return matcher.InstructionEnumeration();
+    }
+
+    public static RandomScrapSpawn[] GetAssignedRandomScrapSpawn(GrabbableObject item)
+    {
+        if (Patch_RoundManager.s_AssignedRandomSpawn.TryGetValue(item, out var spawn))
+        {
+            LethalPerformancePlugin.Instance.Logger.LogDebug("Used RandomScrapSpawn");
+
+            Patch_RoundManager.s_AssignedRandomSpawn.Remove(item);
+
+            return [spawn];
+        }
+
+        return [];
+    }
+}
---
+++ LethalPerformance/Patches/Patch_RoundManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using HarmonyLib;
+using LethalPerformance.Patcher.API;
+
+namespace LethalPerformance.Patches;
+[HarmonyPatch(typeof(RoundManager))]
+internal static class Patch_RoundManager
+{
+    internal static readonly ConditionalWeakTable<GrabbableObject, RandomScrapSpawn> s_AssignedRandomSpawn = new();
+
+    private static RandomScrapSpawn? s_RandomScrapSpawn;
+
+    [HarmonyCleanup]
+    private static Exception? Cleanup(Exception exception)
+    {
+        return HarmonyExceptionHandler.ReportException(exception);
+    }
+
+    [HarmonyPatch(nameof(RoundManager.SpawnScrapInLevel))]
+    [HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> CatchRandomScrapSpawn(IEnumerable<CodeInstruction> instructions)
+    {
+        var matcher = new CodeMatcher(instructions);
+
+        var randomScrapSpawnLocal = matcher.SearchForward(ci => ci.operand is LocalBuilder lb && lb.LocalType == typeof(RandomScrapSpawn))
+            .ThrowIfInvalid("Failed to find localbuilder of RandomScrapSpawn")
+            .Operand as LocalBuilder;
+
+        var spawnWithParentField = typeof(RandomScrapSpawn).GetField(nameof(RandomScrapSpawn.spawnWithParent), AccessTools.all);
+
+        matcher.MatchForward(false,
+            [
+            new(OpCodes.Ldloc_S, randomScrapSpawnLocal),
+            new(OpCodes.Ldfld, spawnWithParentField),
+            new(OpCodes.Callvirt),
+            new(OpCodes.Callvirt),
+            new(OpCodes.Stloc_S),
+            new(OpCodes.Br),
+            ])
+            .ThrowIfInvalid("Failed to find patch point")
+            .Insert(
+            [
+                new(OpCodes.Ldloc, randomScrapSpawnLocal),
+                CodeInstruction.Call(() => CaptureScrapSpawn)
+            ]);
+
+        var grabbableObjectLocal = matcher.SearchForward(ci => ci.operand is LocalBuilder lb && lb.LocalType == typeof(GrabbableObject))
+           .ThrowIfInvalid("Failed to find localbuilder of GrabbableObject")
+           .Operand as LocalBuilder;
+
+        matcher.MatchForward(false,
+            [
+            new(OpCodes.Ldloc_S, grabbableObjectLocal),
+            ])
+            .Insert(
+            [
+                new(OpCodes.Ldloc, grabbableObjectLocal),
+                CodeInstruction.Call(() => AssignScrapSpawnToObject)
+            ]);
+
+        return matcher.InstructionEnumeration();
+    }
+
+    private static void CaptureScrapSpawn(RandomScrapSpawn randomScrapSpawn)
+    {
+        s_RandomScrapSpawn = randomScrapSpawn;
+    }
+
+    private static void AssignScrapSpawnToObject(GrabbableObject grabbableObject)
+    {
+        if (s_RandomScrapSpawn == null)
+        {
+            return;
+        }
+
+        s_AssignedRandomSpawn.AddOrUpdate(grabbableObject, s_RandomScrapSpawn!);
+        s_RandomScrapSpawn = null;
+    }
+}
